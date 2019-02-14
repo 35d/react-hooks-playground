34f4c515e1a8bcfacb69ed6a820565f5a9e34b5f
Yuji Tsuburaya 🌚 <yuji.tsuburaya@bizreach.co.jp>
2019-02-14 13:20:43 +0900

    ✨ reducer sample

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const Application = () => {
-  return <h1>Hello world!</h1>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { getStateAndActions } from './reducer';
+
+const Button = props => (
+  <button type="button" onClick={props.onClick}>
+    {props.children}
+  </button>
+);
+
+const App = () => {
+  const [state, actions] = getStateAndActions();
+  return (
+    <React.Fragment>
+      {state.test}
+      <Button onClick={actions.addTest}>Add 1</Button>
+      <Button onClick={actions.removeTest}>Remove 1</Button>
+      <Button onClick={actions.reset}>Reset</Button>
+    </React.Fragment>
+  );
 };
-ReactDOM.render(<Application />, document.getElementById("app"));
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ reducer.js
@@ -0,0 +1,28 @@
+import { useReducer } from 'react';
+
+const initialState = {
+  test: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'reset':
+      return initialState;
+    case 'addTest':
+      return { test: state.test + 1 };
+    case 'removeTest':
+      return { test: state.test - 1 };
+  }
+};
+
+const mapDispatch = dispatch => ({
+  reset: () => dispatch({ type: 'reset' }),
+  addTest: () => dispatch({ type: 'addTest' }),
+  removeTest: () => dispatch({ type: 'removeTest' })
+});
+
+export const getStateAndActions = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const actions = mapDispatch(dispatch);
+  return [state, actions];
+};
